a6f0e8e4f52eeeeea0c2e9e0333d20774b2c0a58
Ranjana1984 <142005449+Ranjana1984@users.noreply.github.com>
2024-01-03 17:50:31 +0000

    operations type

---
+++ 02_basic/04_operations.js
@@ -0,0 +1,18 @@
+let value = 3
+let negValue = -value
+ //console.log(negValue);
+
+ console.log(3+3);
+ console.log(3-4);
+ console.log(3*5);
+ console.log(3**2);
+ console.log(3/2);
+ console.log(2%3);
+
+ let str1 = "how r u"
+ let str2 = " i am fine"
+
+ let str3 = str1+str2
+ console.log(str3);
+ 
+ 
