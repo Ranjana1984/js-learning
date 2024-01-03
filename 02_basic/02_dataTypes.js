ced12ca3a435e268a9875eabfcc0cf1f6103e42b
Ranjana1984 <142005449+Ranjana1984@users.noreply.github.com>
2024-01-03 16:50:58 +0000

    datatypes

---
+++ 02_basic/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version new standard //
+
+// alert(3+3) // we are using nodejs, not browser 
+
+console.log(3
+    +
+    3) // code should be not written in this format
+
+// console.log("reading code")
+
+let name = "reading code"
+let age = 18 
+let isLoggedIn = false 
+
+// number => 2 to power 53 
+// bigint
+// string => ""
+//boolean => true/false  means only yes or no 
+// null => standalone value means empty value 
+// undefined => means variable is declare but the value is not assign 
+// symbol  for finding unique components 
+
+
+
+//object 
+
+console.log(typeof  null); //object
+
+console.log(typeof  undefined); //undefined
+console.log(typeof  age); 
+console.log(typeof  "code"); 
